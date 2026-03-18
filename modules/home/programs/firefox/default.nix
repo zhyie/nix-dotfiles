b77432b5f7cef50252a6f13cd1adb35c418639c7
zhyie <128297362+zhyie@users.noreply.github.com>
2026-03-19 02:40:35 +0800

    add: firefox policies

--- modules/home/programs/firefox/default.nix
+++ modules/home/programs/firefox/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.firefox = {
     enable = true;
@@ -7,133 +7,106 @@
       "tl"
     ];
 
-    policies = {
-      ## General
-      DontCheckDefaultBrowser = true;
-      DisplayBookmarksToolbar = "newtab";
-      DisplayMenuBar = "default-off";
-      Homepage.StartPage = "previous-session";
-      SanitizedOnShutdown = {
-        Cache = true;
-        Cookies = true;
-        FormData = true;
-        History = true;
-        SiteSettings = true;
-      };
+    ## PROFILES
+    profiles = {
+      ## default
+      default = {
+        id = 0;
+        name = "default";
+        search = {
+          engines = {
+            "Nix Packages" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    {
+                      name = "channel";
+                      value = "unstable";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@np" ];
+            };
 
-      ## Firefox
-      DisableFirefoxAccounts = true;
-      DisableFirefoxStudies = true;
-      DisableTelemetry = true;
-      FirefoxHome = {
-        Search = true;
-        TopSites = false;
-        SponsoredTopSites = false;
-        Highlights = false;
-        Pocket = false;
-        Stories = false;
-        SponsoredPocket = false;
-        SponsoredStories = false;
-        Snippets = false;
-      };
-      FirefoxSuggest = {
-        WebSuggestions = false;
-        SponsoredSuggestions = false;
-        ImprovedSuggest = false;
-      };
-      UserMessaging = {
-        ExtensionRecommendations = false;
-        UrlbarInterventions = false;
-        SkipOnboarding = true;
-        MoreFromMozilla = false;
-        FirefoxLabs = true;
-      };
+            "Nix Options" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/options";
+                  params = [
+                    {
+                      name = "channel";
+                      value = "unstable";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@no" ];
+            };
 
-      ## Search
-      SearchBar = "unified";
-      SearchSuggestEnabled = false;
-      # SearchEngines = {
-      #   Default = "DuckDuckGo";
-      #   Remove = [
-      #     "Google"
-      #     "Bing"
-      #     "eBay"
-      #     "Perplexity"
-      #     "Wikipedia"
-      #   ];
-      # Add = [
-      #   {
-      #     "Name" = "DuckDuckGo";
-      #     "URLTemplate" = "https://duckduckgo.com/?q={searchTerms}";
-      #     "IconURL" = "https://duckduckgo.com/favicon.ico";
-      #     "Alias" = "ddg";
-      #   }
-      #   {
-      #     "Name" = "Startpage";
-      #     "URLTemplate" = "https://www.startpage.com/sp/search?query={searchTerms}";
-      #     "IconURL" = "https://www.startpage.com/favicon.ico";
-      #     "Alias" = "sp";
-      #   }
-      # ];
-      # };
+            "NixOS Wiki" = {
+              urls = [
+                {
+                  template = "https://wiki.nixos.org/w/index.php";
+                  params = [
+                    {
+                      name = "search";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@nw" ];
+            };
 
-      ## Privacy and Security
-      PasswordManagerEnabled = false;
-      EnableTrackingProtection = {
-        Value = true;
-        Locked = true;
-        Cryptomining = true;
-        Fingerprinting = true;
-      };
-      HttpsOnlyMode = "enabled";
-      DNSOverHTTPS = {
-        Enabled = true;
-        ProviderURL = " https://sky.rethinkdns.com/1:AAoACAgA";
-        Fallback = true;
-      };
-      Permissions = {
-        Camera.BlockNewRequests = true;
-        Microphone.BlockNewRequests = true;
-        Location.BlockNewRequests = true;
-        Notification.BlockNewRequests = true;
-        Autoplay.Default = "block-audio";
-        VirtualReality.BlockNewRequests = true;
-        ScreenShare.BlockNewRequests = true;
-      };
-      Cookies = {
-        Allow = [
-          "https://github.com"
-          "https://notion.so"
-        ];
-        AllowSession = [
-          "https://proton.me"
-          "https://tuta.com"
-        ];
-      };
-
-      ## Extensions
-      "3rdParty" = {
-        Extensions = {
-          "uBlock0@raymondhill.net".adminSettings = builtins.readFile ./ublock.txt;
+            "Home Manager" = {
+              urls = [
+                {
+                  template = "https://home-manager-options.extranix.com";
+                  params = [
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                    {
+                      name = "release";
+                      value = "master";
+                    }
+                  ];
+                }
+              ];
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@hm" ];
+            };
+          };
         };
-      };
-      ExtensionSettings = {
-        # ublock origin
-        "uBlock0@raymondhill.net" = {
-          installation_mode = "force_installed";
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-        };
-        # bitwarden
-        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-          installation_mode = "normal_installed";
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-        };
+        userChrome = builtins.readFile ./cascade/userChrome.css;
       };
 
-      ## Preferences
-      Preferences = {
-
+      ## secondary profile
+      profile_1 = {
+        id = 1;
+        name = "profile_1";
+        search = {
+          default = "DuckDuckGo";
+          privateDefault = "DuckDuckGo";
+        };
       };
     };
+
+    ## POLICIES
+    policies = import ./policies.nix;
   };
 }
