6aa1acd7950f244577fac8da257f43b955e0ceae
zhyie <128297362+zhyie@users.noreply.github.com>
2026-03-24 10:39:57 +0800

    feat: extra packages with rofi
    Package rofi with the following:
    - rofi
      - power-menu
      - calc
      - emoji
      - mpd
      - rbw-x11
      - obsidian
    - keepmenu

--- modules/home/programs/rofi/default.nix
+++ modules/home/programs/rofi/default.nix
@@ -1,22 +1,36 @@
 { pkgs, ... }:
+
+let
+  rofi = pkgs.rofi.override {
+    plugins = builtins.attrValues {
+      inherit (pkgs)
+        rofi-power-menu
+        rofi-calc
+        rofi-emoji
+        rofi-mpd
+        rofi-rbw-x11
+        rofi-obsidian
+        keepmenu
+        ;
+    };
+  };
+in
 {
-  home.packages = [ pkgs.rofi ];
-
-  # home.file.".config/rofi" = {
-  #   source = ../../dotfiles/rofi;
-  #   recursive = true;
-  # };
-
+  home.packages = [ rofi ];
   # programs.rofi = {
   #   enable = true;
-  #   modes = [
-  #     "drun"
-  #     "run"
-  #     "window"
-  #   ];
-  #   extraConfig = {
-  #     show-icons = true;
+  #   package = pkgs.rofi.override {
+  #     plugins = builtins.attrValues {
+  #       inherit (pkgs)
+  #         rofi-power-menu
+  #         rofi-calc
+  #         rofi-emoji
+  #         rofi-mpd
+  #         rofi-rbw-x11
+  #         rofi-obsidian
+  #         keepmenu
+  #         ;
+  #     };
   #   };
   # };
-
 }
