1a98e1bc37ed24ba30da472c730cef8fa1db8708
zhyie <128297362+zhyie@users.noreply.github.com>
2026-06-11 18:10:21 +0800

    Remove printer

--- modules/nixos/hardware/printing/epson.nix
+++ modules/nixos/hardware/printing/epson.nix
@@ -1,28 +1,28 @@
-{ pkgs, ... }:
+{ ... }:
 
-let
-  printer = "Epson";
-  model = "L3210";
-in
+# let
+#   printer = "Epson";
+#   model = "L3210";
+# in
 {
   imports = [
     ./printing.nix
     ./scanner.nix
   ];
 
-  services.printing.drivers = [ pkgs.epson-escpr ];
+  # services.printing.drivers = [ pkgs.epson-escpr ];
 
-  hardware = {
-    printers.ensurePrinters = [
-      {
-        name = "${printer}_${model}";
-        model = "";
-        description = "${printer} ${model}";
-        deviceUri = "usb://EPSON/L3210%20Series?serial=5841474D3337313280";
-        location = "Home";
-      }
-    ];
+  # hardware = {
+  #   printers.ensurePrinters = [
+  #     {
+  #       name = "${printer}_${model}";
+  #       model = "";
+  #       description = "${printer} ${model}";
+  #       deviceUri = "usb://EPSON/L3210%20Series?serial=5841474D3337313280";
+  #       location = "Home";
+  #     }
+  #   ];
 
-    sane.extraBackends = [ pkgs.epsonscan2 ];
-  };
+  #   sane.extraBackends = [ pkgs.epsonscan2 ];
+  # };
 }
