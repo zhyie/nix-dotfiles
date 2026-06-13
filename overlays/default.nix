491db3d73fe7876d5d64475965df751248c2ddcf
zhyie <128297362+zhyie@users.noreply.github.com>
2026-06-14 01:43:28 +0800

    Patch for picom package

--- overlays/default.nix
+++ overlays/default.nix
@@ -15,5 +15,14 @@
   modify-packages = _: prev: {
     firefox = import ./firefox.nix { inherit inputs prev; };
     openldap = prev.openldap.overrideAttrs { doCheck = false; };
+
+    picom = prev.picom.overrideAttrs (old: {
+      patches = (old.patches or [ ]) ++ [
+        (prev.fetchpatch {
+          url = "https://github.com/yshui/picom/commit/676196359c15bb654696b05700330685db914710.patch";
+          hash = "sha256-YbpGL6FvXdIh3N5zJ1oJJc/YOLLVQL0Jlp0LetpnecA=";
+        })
+      ];
+    });
   };
 }
